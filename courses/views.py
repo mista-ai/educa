2cf8edd621a95324eb27886ee0a0c5bcf449ec9a
mista <mr.bezdar@gmail.com>
2024-02-24 20:54:03 +0300

    Initial commit

---
+++ courses/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
+
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'course/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
